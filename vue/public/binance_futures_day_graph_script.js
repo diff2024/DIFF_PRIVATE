80977374e3c77f9838d00a39fc40d168c44c5a9f
diff2024 <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>
2023-12-16 14:33:22 +0900

    2023-12-16-1

--- vue/public/binance_futures_day_graph_script.js
+++ vue/public/binance_futures_day_graph_script.js
@@ -25,23 +25,34 @@ function drawChart() {
         var coin_kor_name = ''
         var color = ''
         var o_c_percent = ''
+        var all_color = ''
         
         for(var x=1; x<data_list.length; x++){
             coin_ticker = data_list[x]['coin_ticker'];
             coin_kor_name = data_list[x]['coin_kor_name'];
             color = data_list[x]['color'];
             o_c_percent = data_list[x]['o_c_price_rate'];
-            
             if(x%20==1){
                 rank_x = String(x)
                 dataArray = [];
                 dataArray.push(['X', 'PERCENT', { role: 'style' }, { role: 'annotation' } ]);
                 dataArray.push([btc_coin_ticker, Number(btc_o_c_percent), '#F8941C', String(btc_o_c_percent)]);
                 dataArray.push([coin_ticker, Number(o_c_percent), color, String(o_c_percent)]);
+                
+                if(Number(btc_o_c_percent) > 0 && Number(o_c_percent) > 0){
+                    all_color = color
+                }else if(Number(btc_o_c_percent) < 0 && Number(o_c_percent) < 0){
+                    all_color = color
+                }else{
+                    all_color = ''
+                }
 
             }else if(x%20==0){
+                if(all_color != '' && all_color != color){
+                    all_color = ''
+                }
                 dataArray.push([coin_ticker, Number(o_c_percent), color, String(o_c_percent)]);
-
+                
                 var data = new google.visualization.arrayToDataTable(dataArray);
                 var options = {
                     bar: {groupWidth: "45%"},
@@ -61,14 +72,71 @@ function drawChart() {
                             fontSize: 13,
                             color: 'black',
                             bold: true
-                        }
+                        },
                     },
                     width: 900,
                     height: 400
                 };
+
+                if(all_color == 'red'){
+                    options = {
+                        bar: {groupWidth: "45%"},
+                        chartArea: { width: '90%' },
+                        legend: 'none',
+                        hAxis: {
+                            textStyle: {
+                                fontName: '맑은 고딕',
+                                fontSize: 13,
+                                color: 'black',
+                                bold: true
+                            }
+                        },
+                        vAxis: {
+                            textStyle: {
+                                fontName: '맑은 고딕',
+                                fontSize: 13,
+                                color: 'black',
+                                bold: true
+                            },
+                            viewWindow: { min: 0 }, viewWindowMode: "explicit"
+                        },
+                        width: 900,
+                        height: 400
+                    };
+                }else if(all_color == 'blue'){
+                    options = {
+                        bar: {groupWidth: "45%"},
+                        chartArea: { width: '90%' },
+                        legend: 'none',
+                        hAxis: {
+                            textStyle: {
+                                fontName: '맑은 고딕',
+                                fontSize: 13,
+                                color: 'black',
+                                bold: true
+                            }
+                        },
+                        vAxis: {
+                            textStyle: {
+                                fontName: '맑은 고딕',
+                                fontSize: 13,
+                                color: 'black',
+                                bold: true
+                            },
+                            viewWindow: { max: 0 }, viewWindowMode: "explicit"
+                        },
+                        width: 900,
+                        height: 400
+                    };
+                }
+                
                 var chart = new google.visualization.ColumnChart(document.getElementById('binance_futures_day_rank_'+String(rank_x)+'_graph'));
                 chart.draw(data, options);
             }else{
+                if(all_color != '' && all_color != color){
+                    all_color = ''
+                }
+                
                 dataArray.push([coin_ticker, Number(o_c_percent), color, String(o_c_percent)]);
             }
         }
@@ -91,12 +159,64 @@ function drawChart() {
                     fontSize: 13,
                     color: 'black',
                     bold: true
-                }
+                },
             },
             width: 900,
             height: 400
         };
-        var chart = new google.visualization.ColumnChart(document.getElementById('BINANCE_FUTURES_day_rank_'+String(rank_x)+'_graph'));
+
+        if(all_color == 'red'){
+            options = {
+                bar: {groupWidth: "45%"},
+                chartArea: { width: '90%' },
+                legend: 'none',
+                hAxis: {
+                    textStyle: {
+                        fontName: '맑은 고딕',
+                        fontSize: 13,
+                        color: 'black',
+                        bold: true
+                    }
+                },
+                vAxis: {
+                    textStyle: {
+                        fontName: '맑은 고딕',
+                        fontSize: 13,
+                        color: 'black',
+                        bold: true
+                    },
+                    viewWindow: { min: 0 }, viewWindowMode: "explicit"
+                },
+                width: 900,
+                height: 400
+            };
+        }else if(all_color == 'blue'){
+            options = {
+                bar: {groupWidth: "45%"},
+                chartArea: { width: '90%' },
+                legend: 'none',
+                hAxis: {
+                    textStyle: {
+                        fontName: '맑은 고딕',
+                        fontSize: 13,
+                        color: 'black',
+                        bold: true
+                    }
+                },
+                vAxis: {
+                    textStyle: {
+                        fontName: '맑은 고딕',
+                        fontSize: 13,
+                        color: 'black',
+                        bold: true
+                    },
+                    viewWindow: { max: 0 }, viewWindowMode: "explicit"
+                },
+                width: 900,
+                height: 400
+            };
+        }
+        var chart = new google.visualization.ColumnChart(document.getElementById('binance_futures_day_rank_'+String(rank_x)+'_graph'));
         chart.draw(data, options);
     })
 }
--- vue/public/upbit_day_graph_script.js
+++ vue/public/upbit_day_graph_script.js
@@ -25,6 +25,7 @@ function drawChart() {
         var coin_kor_name = ''
         var color = ''
         var o_c_percent = ''
+        var all_color = ''
         
         for(var x=1; x<data_list.length; x++){
             coin_ticker = data_list[x]['coin_ticker'];
@@ -38,8 +39,19 @@ function drawChart() {
                 dataArray.push(['X', 'PERCENT', { role: 'style' }, { role: 'annotation' } ]);
                 dataArray.push([btc_coin_ticker, Number(btc_o_c_percent), '#F8941C', String(btc_o_c_percent)]);
                 dataArray.push([coin_ticker, Number(o_c_percent), color, String(o_c_percent)]);
+                
+                if(Number(btc_o_c_percent) > 0 && Number(o_c_percent) > 0){
+                    all_color = color
+                }else if(Number(btc_o_c_percent) < 0 && Number(o_c_percent) < 0){
+                    all_color = color
+                }else{
+                    all_color = ''
+                }
 
             }else if(x%20==0){
+                if(all_color != '' && all_color != color){
+                    all_color = ''
+                }
                 dataArray.push([coin_ticker, Number(o_c_percent), color, String(o_c_percent)]);
 
                 var data = new google.visualization.arrayToDataTable(dataArray);
@@ -66,9 +78,66 @@ function drawChart() {
                     width: 900,
                     height: 400
                 };
+
+                if(all_color == 'red'){
+                    options = {
+                        bar: {groupWidth: "45%"},
+                        chartArea: { width: '90%' },
+                        legend: 'none',
+                        hAxis: {
+                            textStyle: {
+                                fontName: '맑은 고딕',
+                                fontSize: 13,
+                                color: 'black',
+                                bold: true
+                            }
+                        },
+                        vAxis: {
+                            textStyle: {
+                                fontName: '맑은 고딕',
+                                fontSize: 13,
+                                color: 'black',
+                                bold: true
+                            },
+                            viewWindow: { min: 0 }, viewWindowMode: "explicit"
+                        },
+                        width: 900,
+                        height: 400
+                    };
+                }else if(all_color == 'blue'){
+                    options = {
+                        bar: {groupWidth: "45%"},
+                        chartArea: { width: '90%' },
+                        legend: 'none',
+                        hAxis: {
+                            textStyle: {
+                                fontName: '맑은 고딕',
+                                fontSize: 13,
+                                color: 'black',
+                                bold: true
+                            }
+                        },
+                        vAxis: {
+                            textStyle: {
+                                fontName: '맑은 고딕',
+                                fontSize: 13,
+                                color: 'black',
+                                bold: true
+                            },
+                            viewWindow: { max: 0 }, viewWindowMode: "explicit"
+                        },
+                        width: 900,
+                        height: 400
+                    };
+                }
+
                 var chart = new google.visualization.ColumnChart(document.getElementById('upbit_day_rank_'+String(rank_x)+'_graph'));
                 chart.draw(data, options);
             }else{
+                if(all_color != '' && all_color != color){
+                    all_color = ''
+                }
+
                 dataArray.push([coin_ticker, Number(o_c_percent), color, String(o_c_percent)]);
             }
         }
@@ -96,6 +165,59 @@ function drawChart() {
             width: 900,
             height: 400
         };
+
+        if(all_color == 'red'){
+            options = {
+                bar: {groupWidth: "45%"},
+                chartArea: { width: '90%' },
+                legend: 'none',
+                hAxis: {
+                    textStyle: {
+                        fontName: '맑은 고딕',
+                        fontSize: 13,
+                        color: 'black',
+                        bold: true
+                    }
+                },
+                vAxis: {
+                    textStyle: {
+                        fontName: '맑은 고딕',
+                        fontSize: 13,
+                        color: 'black',
+                        bold: true
+                    },
+                    viewWindow: { min: 0 }, viewWindowMode: "explicit"
+                },
+                width: 900,
+                height: 400
+            };
+        }else if(all_color == 'blue'){
+            options = {
+                bar: {groupWidth: "45%"},
+                chartArea: { width: '90%' },
+                legend: 'none',
+                hAxis: {
+                    textStyle: {
+                        fontName: '맑은 고딕',
+                        fontSize: 13,
+                        color: 'black',
+                        bold: true
+                    }
+                },
+                vAxis: {
+                    textStyle: {
+                        fontName: '맑은 고딕',
+                        fontSize: 13,
+                        color: 'black',
+                        bold: true
+                    },
+                    viewWindow: { max: 0 }, viewWindowMode: "explicit"
+                },
+                width: 900,
+                height: 400
+            };
+        }
+        
         var chart = new google.visualization.ColumnChart(document.getElementById('upbit_day_rank_'+String(rank_x)+'_graph'));
         chart.draw(data, options);
     })
